a682d309c350e60bc6ce3425e94f9b41a57eb3c2
lx6c78 <dicongyun123@outlook.com>
2026-04-16 22:17:53 +0800

    Update lr_decay.py

--- src/util/lr_decay.py
+++ src/util/lr_decay.py
@@ -15,3 +15,42 @@ def get_layer_id(n, num_layers):
     else:
         return num_layers
 
+def param_groups_lrd(model, weight_decay=0.05, no_weight_decay_list=[], layer_decay=.75):
+    param_group_names = {}
+    param_groups = {}
+    num_layers = sum([len(layer.blocks) for layer in model.mmae_core_block.levels]) + 1 #
+
+    layer_scales = list(layer_decay ** (num_layers - i) for i in range(num_layers + 1))
+
+    for n, p in model.named_parameters():
+        if not p.requires_grad:
+            continue
+
+        if p.ndim == 1 or n in no_weight_decay_list:
+            g_decay = "no_decay"
+            this_decay = 0.
+        else:
+            g_decay = "decay"
+            this_decay = weight_decay
+
+        layer_id = get_layer_id(n, num_layers)
+        group_name = "layer_%d_%s" % (layer_id, g_decay)
+
+        if group_name not in param_group_names:
+            this_scale = layer_scales[layer_id]
+
+            param_group_names[group_name] = {
+                "lr_scale": this_scale,
+                "weight_decay": this_decay,
+                "params": [],
+            }
+            param_groups[group_name] = {
+                "lr_scale": this_scale,
+                "weight_decay": this_decay,
+                "params": [],
+            }
+
+        param_group_names[group_name]["params"].append(n)
+        param_groups[group_name]["params"].append(p)
+
+    return list(param_groups.values())
